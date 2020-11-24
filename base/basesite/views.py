d19f59ff2d49009287ef86fe15c755033af18c7a
CharLie <47771942+Amaranandsr@users.noreply.github.com>
2020-11-24 22:23:04 +0530

    depoly

---
+++ base/basesite/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+#rander for HTML file call
+from django.shortcuts import render
+#HttpResponse For Response code
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+def home(request):
+    return render(request,'pages/home.html')
+def noise(request):
+    return render(request,'pages/denoising.html')
+def super(request):
+    return render(request,'pages/super.html')
+def inpainting(request):
+    return render(request,'pages/inpainting.html')
